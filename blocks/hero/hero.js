c731040ef2c2b3b2c4fee936641d99ab1bf75d26
James Zhou <james.zhou.quan@gmail.com>
2025-02-04 11:39:41 +1100

    add tile

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,24 +1,43 @@
-import { getMetadata } from '../../scripts/aem.js';
+import { getMetadata, createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
-//   const ul = document.createElement('ul');
-//   [...block.children].forEach((row) => {
-//     const li = document.createElement('li');
-//     moveInstrumentation(row, li);
-//     while (row.firstElementChild) li.append(row.firstElementChild);
-//     [...li.children].forEach((div) => {
-//       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-//       else div.className = 'cards-card-body';
-//     });
-//     ul.append(li);
-//   });
-//   ul.querySelectorAll('picture > img').forEach((img) => {
-//     const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
-//     moveInstrumentation(img, optimizedPic.querySelector('img'));
-//     img.closest('picture').replaceWith(optimizedPic);
-//   });
-//   block.textContent = '';
-//   block.append(ul);
+  /* Rearrange and add classes to match hero-result.html structure */
+  const textDiv = document.createElement('div');
+  textDiv.className = 'hero-text';
+  const imageDiv = document.createElement('div');
+  imageDiv.className = 'hero-image';
+
+  [...block.children].forEach((row) => {
+    const innerDiv = row.firstElementChild;
+    if (innerDiv.querySelector('picture')) {
+      // Handle image section
+      moveInstrumentation(row, imageDiv);
+      while (row.firstElementChild) imageDiv.append(row.firstElementChild);
+    } else {
+      // Handle text section
+      moveInstrumentation(row, textDiv);
+      while (row.firstElementChild) textDiv.append(row.firstElementChild);
+      
+      // Add classes to text elements
+      const h1 = textDiv.querySelector('h1');
+      if (h1) h1.className = 'hero-title';
+      
+      const p = textDiv.querySelector('p');
+      if (p) p.className = 'hero-description';
+      
+      const h3 = textDiv.querySelector('h3');
+      if (h3) h3.className = 'hero-link';
+    }
+  });
+
+  // Optimize images
+  imageDiv.querySelectorAll('picture > img').forEach((img) => {
+    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '1170' }]);
+    moveInstrumentation(img, optimizedPic.querySelector('img'));
+    img.closest('picture').replaceWith(optimizedPic);
+  });
+
+  block.textContent = '';
+  block.append(textDiv, imageDiv);
 }
